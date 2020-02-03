f1c6887269efe3b5de3ef41dc491975d56602414
David Rhoden <david@davidrhoden.com>
2020-02-03 07:08:34 -0600

    move fonts around

--- .eleventy.js
+++ .eleventy.js
@@ -54,8 +54,10 @@ module.exports = function(eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy("static/webfonts");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
+
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
