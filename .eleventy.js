1a7da877c7214b560a44e9d9a8c5d15d8534603e
David Rhoden <david@davidrhoden.com>
2020-02-03 07:50:30 -0600

    try full path to webfonts

--- .eleventy.js
+++ .eleventy.js
@@ -54,7 +54,7 @@ module.exports = function(eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
-  eleventyConfig.addPassthroughCopy("static/webfonts/ShaodwGrotesque");
+  eleventyConfig.addPassthroughCopy("static/webfonts/ShadowGrotesque");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
 
