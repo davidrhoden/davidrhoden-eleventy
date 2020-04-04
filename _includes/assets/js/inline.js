6337b78dacc3760af4e887ba6473ae123d310cc0
David Rhoden <david@davidrhoden.com>
2020-04-04 15:17:19 -0500

    work on pagination

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,11 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+$(document).on('click','.next', function(){
+	var painting = $(this).val();
+	console.log(painting);
+	$.get('/paintings/'+ painting +'/index.html', function( data ) {
+	    $('#results').html(data); 
+	});
+});
